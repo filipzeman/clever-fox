0a9f7f84cc95bd37dda3ec60277f6336313097e6
Filip Zeman <zeman.filip@gmail.com>
2021-04-14 10:57:57 +0200

    This is initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.orange,
+          child: Column(
+            children: <Widget>[Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  ''
+              )
+             )],
+          )
+        ),
+      ],
+    ));
+  }
+}
